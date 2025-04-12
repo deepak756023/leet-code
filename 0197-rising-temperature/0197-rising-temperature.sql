6d7b92929ae04f1ee2fcf39cdf7ca6605b2445b1
Deepak Nayak <deepaknayak55260@gmail.com>
2025-04-12 07:07:03 +0530

    Time: 1074 ms (22.26%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,4 +1,6 @@
 # Write your MySQL query statement below
 select w.id
 from Weather w
-where w.temperature > (select temperature from Weather where recordDate = DATE_SUB(w.recordDate, INTERVAL 1 DAY))
+where w.temperature > (select temperature 
+                       from Weather 
+                       where recordDate = DATE_SUB(w.recordDate, INTERVAL 1 DAY));
