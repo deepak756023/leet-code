024256bc50ea83f6de57e5a8d8695c3c088b218e
Deepak Nayak <deepaknayak55260@gmail.com>
2025-05-06 22:25:44 +0530

    Time: 368 ms (41.05%), Space: 0B (100%) - LeetHub

---
+++ 3436-find-valid-emails/3436-find-valid-emails.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT 
+    *
+FROM users
+WHERE email LIKE '%@%.com'
+    AND SUBSTR(email, 1, LOCATE('@', email) - 1) REGEXP '^[a-zA-Z0-9_]+$'
+    AND SUBSTR(email,
+        LOCATE('@', email) + 1,
+        LENGTH(email) - LOCATE('@', email) - 4) REGEXP '^[a-zA-Z]+$'
+ORDER BY user_id;
