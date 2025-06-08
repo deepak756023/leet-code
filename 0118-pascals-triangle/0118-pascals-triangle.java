ba0ac48fc4c16dadc06d1a17a5b5fdd831c8bb96
Deepak Nayak <deepaknayak55260@gmail.com>
2025-06-08 09:14:49 +0530

    Time: 1 ms (84.68%), Space: 42.1 MB (40.67%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -7,7 +7,7 @@ class Solution {
         result.add(initial);
 
         for(int i = 1; i < numRows; i++){
-            List<Integer> temp = new ArrayList<>();
+            List<Integer> temp = new ArrayList<>();// Creating new ArrayList everytime
             temp.add(1);
             for(int j = 1; j < i; j++){
                 int value = result.get(i - 1).get(j - 1) + result.get(i - 1).get(j);
