69009c43c2f0ef2845040ebc9a74ecb2163e4fdf
Deepak Nayak <deepaknayak55260@gmail.com>
2025-07-31 15:27:42 +0530

    Time: 0 ms (100%), Space: 40.7 MB (79.6%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,19 +1,19 @@
-class Solution {
-    public boolean isPowerOfTwo(int n) {
-        if(n <= 0){
-           return false;
-        }
-        while(n % 2 == 0){
-            n /= 2;
-        }
-        return n == 1;
-    }
-}
-
-// public class Solution {
+// class Solution {
 //     public boolean isPowerOfTwo(int n) {
-//         if (n <= 0) return false;
-//         if (n == 1) return true;
-//         return (n % 2 == 0) && isPowerOfTwo(n / 2);
+//         if(n <= 0){
+//            return false;
+//         }
+//         while(n % 2 == 0){
+//             n /= 2;
+//         }
+//         return n == 1;
 //     }
 // }
+
+public class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if (n <= 0) return false;
+        if (n == 1) return true;
+        return (n % 2 == 0) && isPowerOfTwo(n / 2);
+    }
+}
