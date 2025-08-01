e324adffd08c5e5fdd5a50dad8589745bbb837bc
Deepak Nayak <deepaknayak55260@gmail.com>
2025-08-01 09:00:31 +0530

    Time: 4 ms (33.64%), Space: 42.5 MB (36.39%) - LeetHub

--- 3304-find-the-k-th-character-in-string-game-i/3304-find-the-k-th-character-in-string-game-i.java
+++ 3304-find-the-k-th-character-in-string-game-i/3304-find-the-k-th-character-in-string-game-i.java
@@ -3,7 +3,6 @@ class Solution {
         StringBuffer word = new StringBuffer("a");
 
         while(word.length() < k){
-            //var temp = word;
             for(var ch : word.toString().toCharArray()){
                 var next = (char) (ch + 1);
                 word.append(next);
