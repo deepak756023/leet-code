2d7d37ad38a0a5790a614918f22c62b30e12f3a2
Deepak Nayak <deepaknayak55260@gmail.com>
2025-08-01 09:00:10 +0530

    Time: 3 ms (67.63%), Space: 42.3 MB (39.91%) - LeetHub

--- 3304-find-the-k-th-character-in-string-game-i/3304-find-the-k-th-character-in-string-game-i.java
+++ 3304-find-the-k-th-character-in-string-game-i/3304-find-the-k-th-character-in-string-game-i.java
@@ -2,9 +2,9 @@ class Solution {
     public char kthCharacter(int k) {
         StringBuffer word = new StringBuffer("a");
 
-        while (word.length() < k) {
-            // .toString().toCharArray() creates a snapshot, so even though you're appending inside the loop, the current iteration is unaffected — you're not looping over newly added characters.
-            for (var ch : word.toString().toCharArray()) {
+        while(word.length() < k){
+            //var temp = word;
+            for(var ch : word.toString().toCharArray()){
                 var next = (char) (ch + 1);
                 word.append(next);
             }
