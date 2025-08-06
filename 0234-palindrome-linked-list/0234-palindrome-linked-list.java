22211d595a12ca355121f903a646fa6242fba5b7
Deepak Nayak <deepaknayak55260@gmail.com>
2025-08-06 09:56:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        var slow = head;
+        var fast = head;
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        var newHead = new ListNode(slow.next.val);
+
+       var current = slow.next.next;
+        while(current != null){
+            ListNode NL = new ListNode(current.val);
+            NL.next = newHead;
+            newHead = NL;
+            current = current.next;
+            
+        }
+        while(newHead != null){
+            if(newHead.val != head.val){
+                return false;
+            }
+            newHead = newHead.next;
+            head = head.next;
+        }
+        return true;
+    }
+}
