91c105ee57404d48ccf70226ecd93eac611d6e73
Deepak Nayak <deepaknayak55260@gmail.com>
2025-08-11 17:15:58 +0530

    Time: 9 ms (3.71%), Space: 45.4 MB (8.16%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -1,44 +1,38 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+import java.math.BigInteger;
+
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        Long first = (long)0;
-        Long second = (long)0;
+        BigInteger first = BigInteger.ZERO;
+        BigInteger second = BigInteger.ZERO;
         int i = 0, j = 0;
+
         while (l1 != null) {
-            first = first + (long) Math.pow(10, i) * l1.val;
+            first = first.add(BigInteger.valueOf(l1.val).multiply(BigInteger.TEN.pow(i)));
             i++;
             l1 = l1.next;
         }
+
         while (l2 != null) {
-            second = second + (long)Math.pow(10, j) * l2.val;
+            second = second.add(BigInteger.valueOf(l2.val).multiply(BigInteger.TEN.pow(j)));
             j++;
             l2 = l2.next;
         }
-        Long number = first + second;
+
+        BigInteger number = first.add(second);
         ListNode sum = new ListNode(-1);
         ListNode dummy = sum;
 
-        if (number == 0) {
+        if (number.equals(BigInteger.ZERO)) {
             return new ListNode(0);
         }
 
-        while (number > 0) {
-            long rem = number % 10;
-            dummy.next = new ListNode((int)rem); // create node
+        while (number.compareTo(BigInteger.ZERO) > 0) {
+            BigInteger[] divRem = number.divideAndRemainder(BigInteger.TEN);
+            dummy.next = new ListNode(divRem[1].intValue()); // create node
             dummy = dummy.next;
-            number /= 10;
+            number = divRem[0];
         }
 
         return sum.next;
-
     }
 }
