a9eae8d77c819238a2a5ab3c90d2ccad7a1a6f71
Deepak Nayak <deepaknayak55260@gmail.com>
2025-08-29 09:44:43 +0530

    Time: 0 ms (100%), Space: 40.4 MB (87.24%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
@@ -1,25 +1,35 @@
+// class Solution {
+//     public int findTheWinner(int n, int k) {
+//         Queue<Integer> queue = new ArrayDeque<>();
+//         int i = 1;
+//         while (i <= n) {
+//             queue.add(i);
+//             i++;
+//         }
+//         int front = 0;
+//         int count = 1;
+//         while (!queue.isEmpty()) {
+
+//             front = queue.remove();
+//             if (count % k == 0) {
+
+//             } else {
+//                 queue.add(front);
+//             }
+//             count++;
+
+//         }
+//         return front;
+
+//     }
+// }
+
 class Solution {
     public int findTheWinner(int n, int k) {
-        Queue<Integer> queue = new ArrayDeque<>();
-        int i = 1;
-        while (i <= n) {
-            queue.add(i);
-            i++;
+        int winner = 0; // J(1, k) = 0 (0-indexed)
+        for (int i = 2; i <= n; i++) {
+            winner = (winner + k) % i;
         }
-        int front = 0;
-        int count = 1;
-        while (!queue.isEmpty()) {
-
-            front = queue.remove();
-            if (count % k == 0) {
-
-            } else {
-                queue.add(front);
-            }
-            count++;
-
-        }
-        return front;
-
+        return winner + 1; // convert to 1-indexed
     }
 }
