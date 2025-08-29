96e3bb2ec0e08f2144a790a357a710a19c87e3e0
Deepak Nayak <deepaknayak55260@gmail.com>
2025-08-29 09:40:57 +0530

    Time: 3 ms (47.25%), Space: 40.9 MB (36.93%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
@@ -1,25 +1,43 @@
+// class Solution {
+//     public int findTheWinner(int n, int k) {
+//         Queue<Integer> queue = new ArrayDeque<>();
+//         int i = 1;
+//         while (i <= n) {
+//             queue.add(i);
+//             i++;
+//         }
+//         int front = 0;
+//         int count = 1;
+//         while (!queue.isEmpty()) {
+
+//             front = queue.remove();
+//             if (count % k == 0) {
+
+//             } else {
+//                 queue.add(front);
+//             }
+//             count++;
+
+//         }
+//         return front;
+
+//     }
+// }
+
 class Solution {
     public int findTheWinner(int n, int k) {
-        Queue<Integer> queue = new ArrayDeque<>();
-        int i = 1;
-        while (i <= n) {
-            queue.add(i);
-            i++;
+        ArrayList<Integer> circle = new ArrayList<>();
+        for (int i = 1; i <= n; ++i) {
+            circle.add(i);
         }
-        int front = 0;
-        int count = 1;
-        while (!queue.isEmpty()) {
+        int cur_ind = 0;
 
-            front = queue.remove();
-            if (count % k == 0) {
-
-            } else {
-                queue.add(front);
-            }
-            count++;
-
+        while (circle.size() > 1) {
+            int next_to_remove = (cur_ind + k - 1) % circle.size();
+            circle.remove(next_to_remove);
+            cur_ind = next_to_remove;
         }
-        return front;
 
+        return circle.get(0);
     }
 }
