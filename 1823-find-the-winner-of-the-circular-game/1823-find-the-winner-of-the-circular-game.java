6d50b9c11b9ad01033a522857730f4016b7e6c65
Deepak Nayak <deepaknayak55260@gmail.com>
2025-08-29 09:35:13 +0530

    Time: 38 ms (20.44%), Space: 44.6 MB (8.48%) - LeetHub

---
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int findTheWinner(int n, int k) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        int i = 1;
+        while (i <= n) {
+            queue.add(i);
+            i++;
+        }
+        int front = 0;
+        int count = 1;
+        while (!queue.isEmpty()) {
+
+            front = queue.remove();
+            if (count % k == 0) {
+
+            } else {
+                queue.add(front);
+            }
+            count++;
+
+        }
+        return front;
+
+    }
+}
