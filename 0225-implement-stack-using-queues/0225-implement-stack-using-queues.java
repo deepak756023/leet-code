f9a5ce055c6df33c4b861d30536571553a56d205
Deepak Nayak <deepaknayak55260@gmail.com>
2025-09-16 18:25:33 +0530

    Time: 0 ms (100%), Space: 41.3 MB (82.78%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -1,18 +1,19 @@
 class MyStack {
     Queue<Integer> queue1 = new ArrayDeque<>();
     Queue<Integer> queue2 = new ArrayDeque<>();
+    private int top;
 
     public MyStack() {
 
     }
 
     public void push(int x) {
+        top = x;
         queue1.add(x);
 
     }
 
     public int pop() {
-        int top = 0;
         while (!queue1.isEmpty()) {
              top = queue1.remove();
             if (!queue1.isEmpty())
@@ -27,7 +28,6 @@ class MyStack {
     }
 
     public int top() {
-        int top = 0;
         while (!queue1.isEmpty()) {
              top = queue1.remove();
             queue2.add(top);
