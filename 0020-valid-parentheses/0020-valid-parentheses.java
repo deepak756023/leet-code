20988810a7894df7c47a69b3d5c3158296bcc163
Deepak Nayak <deepaknayak55260@gmail.com>
2025-09-16 18:01:59 +0530

    Time: 3 ms (45.79%), Space: 42.3 MB (7.3%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        char[] chars = s.toCharArray();
+
+        for(var ch : chars){
+            if(ch == '(' || ch == '{' || ch == '['){
+                stack.push(ch);
+                continue;
+            }
+            if(ch == ')' || ch == '}' || ch == ']'){
+                if(stack.empty()) return false;
+                char top = stack.pop();
+                if(ch == ')' && top == '(' || ch == '}' && top == '{' || ch == ']' && top == '['){
+                    
+                }else{
+                    return false;
+                }
+            }
+
+        }
+        
+        return stack.empty();
+    }
+}
