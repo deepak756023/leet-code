017a8d5717b346df349c7ae778963c3a24d037c0
Deepak Nayak <deepaknayak55260@gmail.com>
2025-09-16 19:24:58 +0530

    Time: 3 ms (99.75%), Space: 60.6 MB (66.18%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -1,45 +1,32 @@
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        if (head == null || head.next == null) 
-            return true;
-
-        // 1. Find middle of the list (slow will stop at middle)
         ListNode slow = head, fast = head;
+
         while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
 
-        // 2. Reverse the second half of the list
-        ListNode secondHalf = reverseList(slow);
+        ListNode curr = (fast == null) ? slow : slow.next;
+        ListNode prev = null;
+        ListNode next = null;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
 
-        // 3. Compare first and second halves
+            prev = curr;
+            curr = next;
+        }
+
         ListNode p1 = head;
-        ListNode p2 = secondHalf;
-        boolean isPalin = true;
+        ListNode p2 = prev;
+
         while (p2 != null) {
-            if (p1.val != p2.val) {
-                isPalin = false;
-                break;
-            }
+            if (p1.val != p2.val) return false;
             p1 = p1.next;
             p2 = p2.next;
         }
 
-        // // 4. (Optional) Restore list to original form
-        // reverseList(secondHalf);
-
-        return isPalin;
-    }
-
-    private ListNode reverseList(ListNode head) {
-        ListNode prev = null, curr = head;
-        while (curr != null) {
-            ListNode nextNode = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = nextNode;
-        }
-        return prev;
+        return true;
     }
 }
