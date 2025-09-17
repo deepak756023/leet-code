130051d3d1dd1db1852b96805cad23986b4acc69
Deepak Nayak <deepaknayak55260@gmail.com>
2025-09-17 20:30:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,43 +1,21 @@
-// class Solution {
-//     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-//         for(int i = 0; i < nums1.length; i++){
-//             for(int j = 0; j < nums2.length; j++){
-//                 if(nums1[i] == nums2[j]){
-//                     if(j != nums2.length-1 && nums1[i] < nums2[j+1]){
-//                         nums1[i] = nums2[j+1];
-//                     }else{
-//                         nums1[i] = -1;
-//                     }
-//                 }
-//             }
-//         }
-//         return nums1;
-//     }
-// }
-
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] result = new int[nums1.length];
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums2.length - 1; i++)
+            map.put(nums2[i], nums2[i + 1]);
 
         for (int i = 0; i < nums1.length; i++) {
-            int index = -1;
-            for (int j = 0; j < nums2.length; j++) {
-                if (nums2[j] == nums1[i]) {
-                    index = j;
-                    break;
+            if (map.containsKey(nums1[i]))
+                if (nums1[i] < map.get(nums1[i])) {
+                    nums1[i] = map.get(nums1[i]);
+                    continue;
                 }
-            }
-            int nextGreater = -1;
-            for (int j = index + 1; j < nums2.length; j++) {
-                if (nums2[j] > nums1[i]) {
-                    nextGreater = nums2[j];
-                    break;
-                }
-            }
 
-            result[i] = nextGreater;
+            nums1[i] = -1;
         }
 
-        return result;
+        return nums1;
+
     }
 }
