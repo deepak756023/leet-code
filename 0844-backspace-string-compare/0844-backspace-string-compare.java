f467cfce1c304f506194e8267330d0cec4707361
Deepak Nayak <deepaknayak55260@gmail.com>
2025-09-17 22:23:59 +0530

    Time: 1 ms (79%), Space: 41.9 MB (25.82%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -1,35 +1,59 @@
+// class Solution {
+//     public boolean backspaceCompare(String s, String t) {
+//         Stack<Character> stack1 = new Stack<>();
+//         Stack<Character> stack2 = new Stack<>();
+
+//         for (var ch : s.toCharArray()) {
+//             if (ch == '#') {
+//                 if (!stack1.isEmpty())
+//                     stack1.pop();
+//                 continue;
+//             }
+//             stack1.push(ch);
+//         }
+
+//         for (var ch : t.toCharArray()) {
+//             if (ch == '#') {
+//                 if (!stack2.isEmpty())
+//                     stack2.pop();
+//                 continue;
+//             }
+//             stack2.push(ch);
+//         }
+
+//         if (stack1.size() != stack2.size())
+//             return false;
+
+//         while (!stack1.isEmpty()) {
+//             if (stack1.pop() != stack2.pop())
+//                 return false;
+//         }
+
+//         return true;
+
+//     }
+// }
+
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        Stack<Character> stack1 = new Stack<>();
-        Stack<Character> stack2 = new Stack<>();
-
-        for (var ch : s.toCharArray()) {
-            if (ch == '#') {
-                if (!stack1.isEmpty())
-                    stack1.pop();
-                continue;
-            }
-            stack1.push(ch);
+        if (getFinalString(s).equals(getFinalString(t))) {
+            return true;
         }
-
-        for (var ch : t.toCharArray()) {
-            if (ch == '#') {
-                if (!stack2.isEmpty())
-                    stack2.pop();
-                continue;
-            }
-            stack2.push(ch);
-        }
-
-        if (stack1.size() != stack2.size())
-            return false;
-
-        while (!stack1.isEmpty()) {
-            if (stack1.pop() != stack2.pop())
-                return false;
-        }
-
-        return true;
-
+        return false;
     }
+
+    private String getFinalString(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (char ch : s.toCharArray()) {
+            if (ch != '#')
+                sb.append(ch);
+            else {
+                if (sb.length() != 0) {
+                    sb.deleteCharAt(sb.length() - 1);
+                }
+            }
+        }
+        return sb.toString();
+    }
+
 }
