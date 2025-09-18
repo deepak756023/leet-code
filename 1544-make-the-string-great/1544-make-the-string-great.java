234ebd709030d1d7cce77204e194942220c956c2
Deepak Nayak <deepaknayak55260@gmail.com>
2025-09-18 12:58:42 +0530

    Time: 3 ms (48.74%), Space: 42.8 MB (13.86%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.java
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -3,19 +3,25 @@ class Solution {
         if (s.length() < 2) {
             return s;
         }
-        
+
         Stack<Character> stack = new Stack<>();
-        for (char currChar : s.toCharArray()) {
-            if (!stack.isEmpty() && Math.abs(stack.peek() - currChar) == 32) {
-                stack.pop();
-            } else {
-                stack.push(currChar);
+
+        for(var ch : s.toCharArray()){
+            if(!stack.empty()){
+                if(Math.abs(ch - (stack.peek())) == 32){
+                    stack.pop();
+                    continue;
+                }
+               
             }
+             stack.push(ch);
         }
-        
-        StringBuilder ans = new StringBuilder();
-        for (char currChar : stack) 
-            ans.append(currChar);
-        return ans.toString();
+
+        StringBuilder good = new StringBuilder();
+        while(!stack.empty()){
+            good.insert(0, stack.pop());
+        }
+
+        return good.toString();
     }
 }
