7fefbe5acfe1ab6fa6c77de14211db6156b3ab53
Deepak Nayak <deepaknayak55260@gmail.com>
2025-09-18 14:21:45 +0530

    Time: 1 ms (99.67%), Space: 42 MB (86.6%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.java
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -1,27 +1,44 @@
+// class Solution {
+//     public String makeGood(String s) {
+//         if (s.length() < 2) {
+//             return s;
+//         }
+
+//         Stack<Character> stack = new Stack<>();
+
+//         for(var ch : s.toCharArray()){
+//             if(!stack.empty()){
+//                 if(Math.abs(ch - (stack.peek())) == 32){
+//                     stack.pop();
+//                     continue;
+//                 }
+               
+//             }
+//              stack.push(ch);
+//         }
+
+//         StringBuilder good = new StringBuilder();
+//         while(!stack.empty()){
+//             good.insert(0, stack.pop());
+//         }
+
+//         return good.toString();
+//     }
+// }
+
 class Solution {
     public String makeGood(String s) {
-        if (s.length() < 2) {
-            return s;
-        }
-
-        Stack<Character> stack = new Stack<>();
-
-        for(var ch : s.toCharArray()){
-            if(!stack.empty()){
-                if(Math.abs(ch - (stack.peek())) == 32){
-                    stack.pop();
-                    continue;
-                }
-               
+        StringBuilder ans = new StringBuilder();
+        for(char ch: s.toCharArray()){
+            int len = ans.length();
+            if(len>0 && Math.abs(ans.charAt(len-1) -ch) == 32)
+            {
+                ans.deleteCharAt(len-1);
+            }else {
+                ans.append(ch);
             }
-             stack.push(ch);
         }
 
-        StringBuilder good = new StringBuilder();
-        while(!stack.empty()){
-            good.insert(0, stack.pop());
-        }
-
-        return good.toString();
+        return ans.toString();
     }
 }
