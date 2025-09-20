97ff67b3e216f709c0087a712057c932bea30145
Deepak Nayak <deepaknayak55260@gmail.com>
2025-09-20 09:41:57 +0530

    Time: 0 ms (100%), Space: 41.9 MB (47.43%) - LeetHub

---
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String reversePrefix(String word, char ch) {
+        StringBuilder str = new StringBuilder();
+        if(word.contains(String.valueOf(ch))){
+            int index = word.indexOf(String.valueOf(ch));
+            str.append(word.substring(0, index + 1));
+            return str.reverse().toString().concat(word.substring(index + 1));
+
+        }
+        return word;
+        
+    }
+}
