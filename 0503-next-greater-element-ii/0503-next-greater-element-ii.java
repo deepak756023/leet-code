31bf6ac07512c5448d4061f1a1155a7898d81b9d
Deepak Nayak <deepaknayak55260@gmail.com>
2025-09-25 11:23:20 +0530

    Time: 13 ms (75.57%), Space: 47.1 MB (5.14%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,21 +1,20 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        Stack<Integer> stack = new Stack<>();
+        Map<Integer, Integer> map = new HashMap<>();//<Index, greaterNumber>
+        Stack<Integer> stack = new Stack<>();//<Index>
         int len = nums.length;
 
         for (int i = 0; i < 2 * len; i++) {
             int j = i % len;
             int num = nums[j];
 
-            // check against indices stored in stack
             while (!stack.isEmpty() && nums[stack.peek()] < num) {
                 int idx = stack.pop();
-                map.put(idx, num); // store next greater for index idx
+                map.put(idx, num);
             }
 
             if (!map.containsKey(j)) {
-                stack.push(j); // push index, not value
+                stack.push(j);
             }
         }
 
