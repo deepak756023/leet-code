74980fc6077b2fef35f294fba41b2d1aae1265b2
Deepak Nayak <deepaknayak55260@gmail.com>
2025-10-18 23:20:09 +0530

    Time: 0 ms (100%), Space: 41.6 MB (81.67%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -1,59 +1,48 @@
-// class Solution {
-//     public boolean backspaceCompare(String s, String t) {
-//         Stack<Character> stack1 = new Stack<>();
-//         Stack<Character> stack2 = new Stack<>();
-
-//         for (var ch : s.toCharArray()) {
-//             if (ch == '#') {
-//                 if (!stack1.isEmpty())
-//                     stack1.pop();
-//                 continue;
-//             }
-//             stack1.push(ch);
-//         }
-
-//         for (var ch : t.toCharArray()) {
-//             if (ch == '#') {
-//                 if (!stack2.isEmpty())
-//                     stack2.pop();
-//                 continue;
-//             }
-//             stack2.push(ch);
-//         }
-
-//         if (stack1.size() != stack2.size())
-//             return false;
-
-//         while (!stack1.isEmpty()) {
-//             if (stack1.pop() != stack2.pop())
-//                 return false;
-//         }
-
-//         return true;
-
-//     }
-// }
-
 class Solution {
-    public boolean backspaceCompare(String s, String t) {
-        if (getFinalString(s).equals(getFinalString(t))) {
-            return true;
-        }
-        return false;
-    }
-
-    private String getFinalString(String s) {
-        StringBuilder sb = new StringBuilder();
-        for (char ch : s.toCharArray()) {
-            if (ch != '#')
-                sb.append(ch);
-            else {
-                if (sb.length() != 0) {
-                    sb.deleteCharAt(sb.length() - 1);
-                }
+        public boolean backspaceCompare(String S, String T) {
+        int i = S.length() - 1, j = T.length() - 1, back;
+        while (true) {
+            back = 0;
+            while (i >= 0 && (back > 0 || S.charAt(i) == '#')) {
+                back += S.charAt(i) == '#' ? 1 : -1;
+                i--;
+            }
+            back = 0;
+            while (j >= 0 && (back > 0 || T.charAt(j) == '#')) {
+                back += T.charAt(j) == '#' ? 1 : -1;
+                j--;
+            }
+            if (i >= 0 && j >= 0 && S.charAt(i) == T.charAt(j)) {
+                i--;
+                j--;
+            } else {
+                break;
             }
         }
-        return sb.toString();
+        return i == -1 && j == -1;
     }
-
 }
+
+// class Solution {
+//     public boolean backspaceCompare(String s, String t) {
+//         if (getFinalString(s).equals(getFinalString(t))) {
+//             return true;
+//         }
+//         return false;
+//     }
+
+//     private String getFinalString(String s) {
+//         StringBuilder sb = new StringBuilder();
+//         for (char ch : s.toCharArray()) {
+//             if (ch != '#')
+//                 sb.append(ch);
+//             else {
+//                 if (sb.length() != 0) {
+//                     sb.deleteCharAt(sb.length() - 1);
+//                 }
+//             }
+//         }
+//         return sb.toString();
+//     }
+
+// }
