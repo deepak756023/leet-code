725d30442fcf020822bc35b7eb4a2a9250a74277
Deepak Nayak <deepaknayak55260@gmail.com>
2025-10-18 22:55:11 +0530

    Time: 3 ms (99.76%), Space: 59.3 MB (73.14%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -1,13 +1,14 @@
 class Solution {
     public boolean isPalindrome(ListNode head) {
         ListNode slow = head, fast = head;
-
+        //finding the middle node
         while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
-
         ListNode curr = (fast == null) ? slow : slow.next;
+
+        //InPlace revrsal
         ListNode prev = null;
         ListNode next = null;
         while (curr != null) {
@@ -17,12 +18,13 @@ class Solution {
             prev = curr;
             curr = next;
         }
-
+        //checking the first half with second
         ListNode p1 = head;
         ListNode p2 = prev;
 
         while (p2 != null) {
-            if (p1.val != p2.val) return false;
+            if (p1.val != p2.val)
+                return false;
             p1 = p1.next;
             p2 = p2.next;
         }
