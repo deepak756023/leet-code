6f0694c26ab776776b85d64ca54b4d3b16246069
Deepak Nayak <deepaknayak55260@gmail.com>
2025-10-20 09:02:33 +0530

    Time: 17 ms (15.57%), Space: 45.3 MB (78.24%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
@@ -2,14 +2,14 @@ class Solution {
     public String reverseWords(String s) {
         String op = "";
         int start = 0;
-        int next = 1;
+        int next = 0;
         int length = s.length();
 
         while (next < length) {
             if (s.charAt(next) == ' ') {
                 op = op + reverse(s.substring(start, next)) + " ";
                 start = next + 1;
-                next = start + 1;
+                next = start;
             }
             if (next == length - 1) {
                 op = op + reverse(s.substring(start, next + 1));
